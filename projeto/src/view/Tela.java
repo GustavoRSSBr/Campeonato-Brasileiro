712506cbf4e2428750f2734c8a3ffdcc3a86c76d
Douglinhas <daguiaralcantara@gmail.com>
2023-10-09 16:09:16 -0300

    Implementação da Tela.

--- projeto/src/view/Tela.java
+++ projeto/src/view/Tela.java
@@ -14,7 +14,18 @@ public class Tela {
 		try {
 			Controle controle = new Controle();
 
-			System.out.println(controle.partidaComMaisGols());
+			System.out.println("--  Bem vindos às estatisticas do Brasileirão de pontos corridos  --");
+
+			System.out.println();
+
+			System.out.println("O time que mais venceu partidas em 2008 foi: " + controle.timeComVitoriasEm2008());
+			System.out.println("O Estado que sediou menos partidas foi: " + controle.estadoComMenosJogos());
+			System.out.println("O jogador com mais gols é: " + controle.jogadorComMaisGols());
+			System.out.println("O jogador com mais gols de penâlti é: " + controle.jogadorComMaisGolsDePenalti());
+			System.out.println("O jogador com mais gols contra é: " + controle.jogadorComMaisGolsContra());
+			System.out.println("O jogador que mais recebeu cartões amarelos foi: " + controle.jogadorComMaisCartoesAmarelos());
+			System.out.println("O jogador que mais recebeu cartões vermelhos foi: " + controle.jogadorComMaisCartoesVermelhos());
+			System.out.println("A partida com o maior placar teve o placar: " + controle.partidaComMaisGols());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
